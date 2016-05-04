8a1972df8de1b8155ea02cf468c128808760b1c4
jakesnyder-hansen <j.snyderhansen.321@gmail.com>
2016-05-03 19:56:51 -0500

    SetUp

---
+++ src/GeneralHashMap.java
@@ -0,0 +1,31 @@
+public class GeneralHashMap extends MyHashMap {
+	
+	public GeneralHashMap() {
+		
+		// TODO: IMPLEMENT CONSTRUCTOR
+
+	}
+
+	@Override
+	protected int hash(String token) {
+		
+		// TODO: IMPLEMENT HASHING FUNCTION FOR GENERAL HASHMAP
+		return -1;
+		
+	}
+
+	@Override
+	public void add(String token) {
+		
+		// TODO: IMPLEMENT ADD METHOD USING BUCKETS
+	
+	}
+
+	@Override
+	public void display() {
+		
+		// TODO: IMPLEMENT DISPLAY METHOD TO SHOW CONTENTS OF ALL BUCKETS
+		
+	}
+
+}
---
+++ src/SpecificHashMap.java
@@ -0,0 +1,32 @@
+public class SpecificHashMap extends MyHashMap {
+
+	public SpecificHashMap() {
+		
+		// TODO: IMPLEMENT CONSTRUCTOR
+
+	}
+
+	@Override
+	protected int hash(String token) {
+		
+		// TODO: IMPLEMENT HASHING FUNCTION FOR GENERAL HASHMAP
+		
+		return -1;
+	}
+
+	@Override
+	public void add(String token) {
+		
+		// TODO: IMPLEMENT ADD METHOD USING BUCKETS
+
+	
+	}
+
+	@Override
+	public void display() {
+		
+		// TODO: IMPLEMENT DISPLAY METHOD TO SHOW CONTENTS OF ALL BUCKETS
+		
+	}
+	
+}
