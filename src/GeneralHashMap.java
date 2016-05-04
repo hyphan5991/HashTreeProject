15d968fa02216a030d7ca81ce9be7ffa6b6b9ca1
roels005 <roels005@umn.edu>
2016-05-03 23:35:05 -0500

    first push

--- src/GeneralHashMap.java
+++ src/GeneralHashMap.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class GeneralHashMap extends MyHashMap {
-	
+
+
+
 	public GeneralHashMap() {
-		
+		LinkedList<String>[] buckets = new LinkedList[131];
+		for (int i = 0; i < 131; i++)
+
+		{
+			buckets[i] = new LinkedList<String>();
+		}
 		// TODO: IMPLEMENT CONSTRUCTOR
 
 	}
@@ -16,13 +26,20 @@ public class GeneralHashMap extends MyHashMap {
 
 	@Override
 	public void add(String token) {
-		
 		// TODO: IMPLEMENT ADD METHOD USING BUCKETS
+		int index = hash(token);
+		buckets[index].add(token);
 	
 	}
 
 	@Override
 	public void display() {
+		for(int i = 0; i<buckets.length;i++){
+			System.out.println("bucket index " + i);
+			for(int j = 0; j<buckets[i].size(); j++){
+				System.out.print(buckets[i].get(j));
+			}
+		}
 		
 		// TODO: IMPLEMENT DISPLAY METHOD TO SHOW CONTENTS OF ALL BUCKETS
 		
--- src/SpecificHashMap.java
+++ src/SpecificHashMap.java
@@ -1,6 +1,14 @@
+import java.util.LinkedList;
+
 public class SpecificHashMap extends MyHashMap {
 
 	public SpecificHashMap() {
+		LinkedList<String>[] buckets = new LinkedList[131];
+		for (int i = 0; i < 131; i++)
+
+		{
+			buckets[i] = new LinkedList<String>();
+		}
 		
 		// TODO: IMPLEMENT CONSTRUCTOR
 
@@ -18,13 +26,19 @@ public class SpecificHashMap extends MyHashMap {
 	public void add(String token) {
 		
 		// TODO: IMPLEMENT ADD METHOD USING BUCKETS
-
+		int index = hash(token);
+		buckets[index].add(token);
 	
 	}
 
 	@Override
 	public void display() {
-		
+		for(int i = 0; i<buckets.length;i++){
+			System.out.println("bucket index " + i);
+			for(int j = 0; j<buckets[i].size(); j++){
+				System.out.print(buckets[i].get(j));
+			}
+		}
 		// TODO: IMPLEMENT DISPLAY METHOD TO SHOW CONTENTS OF ALL BUCKETS
 		
 	}
